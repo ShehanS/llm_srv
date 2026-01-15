4cc18a3addb3fae29bdd3dc1d1fccaa38b3219b0
shehan <shehan.salinda92@gmail.com>
2026-01-15 19:28:57 +0530

    add intelligentSrvUrl fro config

--- src/main/java/com/shehan/llmsvr/event/EventReloadListener.java
+++ src/main/java/com/shehan/llmsvr/event/EventReloadListener.java
@@ -4,6 +4,7 @@ import com.shehan.llmsvr.config.DynamicRouteHolder;
 import com.shehan.llmsvr.config.HttpRouteBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,6 +13,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 @Slf4j
 public class EventReloadListener {
+    @Value("${intelligent-srv.url}")
+    private String intelligentSrvUrl;
 
     private final HttpRouteBuilder builder;
     private final DynamicRouteHolder holder;
@@ -30,7 +33,7 @@ public class EventReloadListener {
     @EventListener
     public void onConfigChanged(ConfigChangedEvent event) {
         log.info("Configuration change detected for route: {}. Notifying AI Service...");
-        String aiServiceUrl = "http://localhost:8500/api/v1/reload";
+        String aiServiceUrl = intelligentSrvUrl + "/api/v1/reload";
 
         webClientBuilder.build()
                 .post()
