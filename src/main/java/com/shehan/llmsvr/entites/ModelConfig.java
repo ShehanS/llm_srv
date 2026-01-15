382d41c02356b7d5a135b3b64e0a3d76ec98f838
shehan <shehan.salinda92@gmail.com>
2026-01-15 10:01:36 +0530

    update

--- src/main/java/com/shehan/llmsvr/entites/ModelConfig.java
+++ src/main/java/com/shehan/llmsvr/entites/ModelConfig.java
@@ -3,26 +3,28 @@ package com.shehan.llmsvr.entites;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-public class ModelConfig  implements Serializable {
+public class ModelConfig implements Serializable {
 
     @Column(name = "model_provider")
-    private String provider;
+    private String provider; // e.g., "openai" or "anthropic"
 
     @Column(name = "model_name")
-    private String name;
+    private String name; // e.g., "gpt-4o" or "claude-3-5-sonnet-20240620"
 
     @Column(name = "temperature")
-    private Double temperature;
+    private Double temperature; // Default 0.0
 
-    @Column(name = "api_key")
-    private String apiKey;
+    @Column(name = "api_key", columnDefinition = "TEXT")
+    private String apiKey; // Optional: Override the .env key if provided
 }
